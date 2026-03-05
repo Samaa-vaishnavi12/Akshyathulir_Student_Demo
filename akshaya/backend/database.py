09412439159256b634c47c3cda44c28501cfc193
Samaa-vaishnavi12 <samaavaishnavi12@gmail.com>
2026-03-05 11:41:38 +0530

    withbackend

--- akshaya/backend/database.py
+++ akshaya/backend/database.py
@@ -7,17 +7,19 @@ client = MongoClient("mongodb://localhost:27017")
 db = client["expertDB"]
 experts = db["experts"]
 counters = db["counters"]
-
+dashboard_collection = db["dashboard_data"]
+mentees_collection = db["mentees_data"]
+revenue_collection = db["revenue_data"]
+availability_dashboard = db["availability_dashboard"]
+resource_collection = db["resources_data"]
 print("Connected to MongoDB")
-
 # ---------------- FOLDERS ----------------
 PROFILE_DIR = "uploads/profiles"
 CERT_DIR = "uploads/certificates"
 
 os.makedirs(PROFILE_DIR, exist_ok=True)
 os.makedirs(CERT_DIR, exist_ok=True)
-
-# ---------------- UNIQUE EXPERT ID ----------------
+#---------------- COUNTERS ----------------#
 def generate_expert_id():
     now = datetime.now()
     yy = now.strftime("%y")
